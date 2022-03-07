0e1c2cd3f6edb8dc8540948ceaa6a31d6112f33e
ZuiYing <63988997+baijianhuas@users.noreply.github.com>
2022-03-07 16:30:22 +0800

    修改框架

--- src/main/resources/sql/feedback.sql
+++ src/main/resources/sql/feedback.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50716
 File Encoding         : 65001
 
-Date: 2017-12-12 20:02:32
+Date: 2022-12-12 20:02:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -46,8 +46,8 @@ CREATE TABLE `feedback` (
 -- ----------------------------
 -- Records of feedback
 -- ----------------------------
-INSERT INTO `feedback` VALUES ('1', '2', '3 4', '4', '1', 'MySQL数据库删除后的恢复工作 - CSDN博客', '[原创]作者：rogerzhanglijie - 来源：csdn - 发表时间：2014年07月17日\r\n\r\n上午不小心把昨天刚刚建好的一个数据库删了个精光!幸好mysql中开启了日志功能。            下面总结一下数据库删除后的恢复方法:...\r\nfeedback.csdn.net/rogerzha...  - 百度快照', '相关搜索', '2017-12-12 19:29:19', '2017-12-12 19:54:28', '百度知道', '0', '0', '0', '0', '0', '0', '0');
-INSERT INTO `feedback` VALUES ('2', '1', '1 2', '1 2 3', '2', '性跟 dbcp 连接池的差不多', '建议配置为true，不影响性能，并且保证安全性。申请连接的时候检测，如果空闲时间大于timeBetweenEvictionRunsMillis，执行validationQuery检测连接是否有效。\r\ntimeBetweenEvictionRunsMillis	 	有两个含义：\r\n1) Destroy线程会检测连接的间隔时间 2) testWhileIdle的判断依据，详细看testWhileIdle属性的说明', ' testWhileIdle的判断依据，详细看testWhileIdle属性的说明', '2017-12-12 19:26:45', '2017-12-12 19:26:49', 'true time millis', '0', '0', '0', '0', '0', '0', '0');
+INSERT INTO `feedback` VALUES ('1', '2', '3 4', '4', '1', 'MySQL数据库删除后的恢复工作 - CSDN博客', '[原创]作者：rogerzhanglijie - 来源：csdn - 发表时间：2014年07月17日\r\n\r\n上午不小心把昨天刚刚建好的一个数据库删了个精光!幸好mysql中开启了日志功能。            下面总结一下数据库删除后的恢复方法:...\r\nfeedback.csdn.net/rogerzha...  - 百度快照', '相关搜索', '2022-12-12 19:29:19', '2022-12-12 19:54:28', '百度知道', '0', '0', '0', '0', '0', '0', '0');
+INSERT INTO `feedback` VALUES ('2', '1', '1 2', '1 2 3', '2', '性跟 dbcp 连接池的差不多', '建议配置为true，不影响性能，并且保证安全性。申请连接的时候检测，如果空闲时间大于timeBetweenEvictionRunsMillis，执行validationQuery检测连接是否有效。\r\ntimeBetweenEvictionRunsMillis	 	有两个含义：\r\n1) Destroy线程会检测连接的间隔时间 2) testWhileIdle的判断依据，详细看testWhileIdle属性的说明', ' testWhileIdle的判断依据，详细看testWhileIdle属性的说明', '2022-12-12 19:26:45', '2022-12-12 19:26:49', 'true time millis', '0', '0', '0', '0', '0', '0', '0');
 
 -- ----------------------------
 -- Table structure for `user_account`
@@ -68,9 +68,9 @@ CREATE TABLE `user_account` (
 -- ----------------------------
 -- Records of user_account
 -- ----------------------------
-INSERT INTO `user_account` VALUES ('1', '1', 'duan', 'duan123456', '我就是不一样的烟火', '2017-12-12 18:05:32');
-INSERT INTO `user_account` VALUES ('2', '2', 'jack', 'jack123456', 'haha', '2017-12-12 18:28:33');
-INSERT INTO `user_account` VALUES ('3', '1', 'lorse', 'lorse123456', '呵呵', '2017-12-12 18:28:53');
+INSERT INTO `user_account` VALUES ('1', '1', 'duan', 'duan123456', '我就是不一样的烟火', '2022-12-12 18:05:32');
+INSERT INTO `user_account` VALUES ('2', '2', 'jack', 'jack123456', 'haha', '2022-12-12 18:28:33');
+INSERT INTO `user_account` VALUES ('3', '1', 'lorse', 'lorse123456', '呵呵', '2022-12-12 18:28:53');
 
 -- ----------------------------
 -- Table structure for `user_link`
@@ -117,9 +117,9 @@ CREATE TABLE `user_picture` (
 -- ----------------------------
 -- Records of user_picture
 -- ----------------------------
-INSERT INTO `user_picture` VALUES ('1', '1', '我的头像', '1', 'C:\\Users\\Administrator\\Desktop\\pic\\image.jpg', 'RWB', '2017-12-12 18:34:11');
-INSERT INTO `user_picture` VALUES ('2', '1', '我的link友情链接GitHub', '2', 'D:\\Duan\\图片\\Image 086.jpg', 'Github图标', '2017-12-12 18:36:54');
-INSERT INTO `user_picture` VALUES ('3', '1', '我的link友情链接CSDN', '0', 'D:\\Duan\\图片', 'CSDN图标', '2017-12-12 18:39:19');
+INSERT INTO `user_picture` VALUES ('1', '1', '我的头像', '1', 'C:\\Users\\Administrator\\Desktop\\pic\\image.jpg', 'RWB', '2022-12-12 18:34:11');
+INSERT INTO `user_picture` VALUES ('2', '1', '我的link友情链接GitHub', '2', 'D:\\Duan\\图片\\Image 086.jpg', 'Github图标', '2022-12-12 18:36:54');
+INSERT INTO `user_picture` VALUES ('3', '1', '我的link友情链接CSDN', '0', 'D:\\Duan\\图片', 'CSDN图标', '2022-12-12 18:39:19');
 
 -- ----------------------------
 -- Table structure for `user_profile`
@@ -179,10 +179,10 @@ CREATE TABLE `feedback_category` (
 -- ----------------------------
 -- Records of feedback_category
 -- ----------------------------
-INSERT INTO `feedback_category` VALUES ('1', '1', '编程语言', 'java c c++ ', '2017-12-12 18:46:52');
-INSERT INTO `feedback_category` VALUES ('2', '1', '网络', 'TCP/IP，UDP，4-7', '2017-12-12 18:47:25');
-INSERT INTO `feedback_category` VALUES ('3', '2', '编程', 'coding', '2017-12-12 19:24:36');
-INSERT INTO `feedback_category` VALUES ('4', '2', 'AI', 'alpha Go', '2017-12-12 19:25:04');
+INSERT INTO `feedback_category` VALUES ('1', '1', '编程语言', 'java c c++ ', '2022-12-12 18:46:52');
+INSERT INTO `feedback_category` VALUES ('2', '1', '网络', 'TCP/IP，UDP，4-7', '2022-12-12 18:47:25');
+INSERT INTO `feedback_category` VALUES ('3', '2', '编程', 'coding', '2022-12-12 19:24:36');
+INSERT INTO `feedback_category` VALUES ('4', '2', 'AI', 'alpha Go', '2022-12-12 19:25:04');
 
 -- ----------------------------
 -- Table structure for `feedback_collect`
@@ -247,8 +247,8 @@ CREATE TABLE `feedback_label` (
 -- ----------------------------
 -- Records of feedback_label
 -- ----------------------------
-INSERT INTO `feedback_label` VALUES ('1', '1', 'java', '2017-12-12 18:41:30');
-INSERT INTO `feedback_label` VALUES ('2', '1', 'android', '2017-12-12 18:42:03');
-INSERT INTO `feedback_label` VALUES ('3', '1', 'TCP/IP', '2017-12-12 18:42:16');
-INSERT INTO `feedback_label` VALUES ('4', '2', 'javaWeb', '2017-12-12 18:42:32');
-INSERT INTO `feedback_label` VALUES ('5', '3', 'MPI', '2017-12-12 18:42:45');
+INSERT INTO `feedback_label` VALUES ('1', '1', 'java', '2022-12-12 18:41:30');
+INSERT INTO `feedback_label` VALUES ('2', '1', 'android', '2022-12-12 18:42:03');
+INSERT INTO `feedback_label` VALUES ('3', '1', 'TCP/IP', '2022-12-12 18:42:16');
+INSERT INTO `feedback_label` VALUES ('4', '2', 'javaWeb', '2022-12-12 18:42:32');
+INSERT INTO `feedback_label` VALUES ('5', '3', 'MPI', '2022-12-12 18:42:45');
